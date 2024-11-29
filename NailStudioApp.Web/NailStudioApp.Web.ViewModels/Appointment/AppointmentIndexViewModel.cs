63808737e0eff12e3d41e8220364278237e12206
Iveliya <iii_koleva@abv.bg>
2024-11-29 09:11:35 +0200

    Update-Controller
    Iveliya

---
+++ NailStudioApp.Web/NailStudioApp.Web.ViewModels/Appointment/AppointmentIndexViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NailStudioApp.Web.ViewModels.Appointment
+{
+    public class AppointmentIndexViewModel
+    {
+        public int Id { get; set; }
+        public string ClientName { get; set; } = string.Empty;
+        public string EmployeeName { get; set; } = string.Empty;
+        public DateTime AppointmentDate { get; set; }
+        public decimal TotalPrice { get; set; }
+        public string Status { get; set; } = "Pending";
+
+    }
+}
---
+++ NailStudioApp.Web/NailStudioApp.Web/Controllers/AppointmentController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NailStudioApp.Data;
+using NailStudioApp.Data.Models;
+using NailStudioApp.Web.ViewModels.Appointment;
+
+namespace NailStudioApp.Web.Controllers
+{
+    public class AppointmentController : Controller
+    {
+        private readonly NailStudioDbContext dbContext;
+        public AppointmentController(NailStudioDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+        
+        public IActionResult Index()
+        {
+            IEnumerable<AppointmentIndexViewModel> appointment = this.dbContext
+                .Appointments
+                .Select(a=>new AppointmentIndexViewModel()
+                {
+                    Id = a.Id,
+                    ClientName=a.Client.FirstName,
+                    EmployeeName=a.Employee.FirstName,
+                    AppointmentDate=a.AppointmentDate,
+                    TotalPrice=a.TotalPrice,
+                    Status=a.Status
+                })
+                .ToArray();
+
+            return View(appointment);
+        }
+    }
+}
